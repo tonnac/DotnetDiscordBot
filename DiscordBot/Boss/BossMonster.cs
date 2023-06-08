e1107a503ccf8cbb77946469cbaabf20b9ea6fda
kimkr <rhkd2060@naver.com>
2023-06-08 16:39:50 +0900

    feat: boss game

---
+++ DiscordBot/Boss/BossMonster.cs
@@ -0,0 +1,106 @@
+﻿using DisCatSharp.Enums;
+
+namespace DiscordBot.Boss;
+
+public enum BossType : int
+{
+    Bat = 0,
+    Octopus = 1,
+    Shark = 2,
+    Unicorn = 3,
+    Mammoth = 4,   
+    Devil = 5,
+    Vampire = 6,
+    Dragon = 7,
+    End,
+}
+
+public class BossMonster
+{
+    public string BossEmojiCode { get; set; }
+    public int CurrentHp { get; set; }
+    public int CurrentMaxHp { get; set; }
+    public int HitCount { get; set; }
+    
+    public BossMonster(BossType type)
+    {
+        SetBossMonsterInfo(type);
+    }
+
+    private void SetBossMonsterInfo(BossType type)
+    {
+        BossEmojiCode = GetBossEmojiCode(type);
+        CurrentHp = GetBossMaxHp(type);
+        CurrentMaxHp = CurrentHp;
+        HitCount = 0;
+    }
+
+    public bool IsKilledByDamage(int damage)
+    {
+        HitCount++;
+        CurrentHp -= damage;
+
+        if (0 >= CurrentHp)
+        {
+            ResetBossMonster();
+            return true;
+        }
+
+        return false;
+    }
+
+    private void ResetBossMonster()
+    {
+        var rand = new Random();
+        int bossType = rand.Next((int) BossType.Bat, (int) BossType.End);
+        SetBossMonsterInfo( (BossType)bossType );
+    }
+
+    private int GetBossMaxHp(BossType type)
+    {
+        switch (type)
+        {
+            default:
+            case BossType.Bat:
+                return 100;
+            case BossType.Octopus:
+                return 200;
+            case BossType.Shark:
+                return 300;
+            case BossType.Unicorn:
+                return 400;
+            case BossType.Mammoth:
+                return 500;
+            case BossType.Devil:
+                return 666;
+            case BossType.Vampire:
+                return 700;
+            case BossType.Dragon:
+                return 1000;
+        }
+    }
+    
+    private string GetBossEmojiCode(BossType type)
+    {
+        switch (type)
+        {
+            default:
+            case BossType.Bat:
+                return "\uD83E\uDD87";
+            case BossType.Octopus:
+                return "\uD83D\uDC19";
+            case BossType.Shark:
+                return "\uD83E\uDD88";
+            case BossType.Unicorn:
+                return "\uD83E\uDD84";
+            case BossType.Mammoth:
+                return "\uD83E\uDDA3";
+            case BossType.Devil:
+                return "\uD83D\uDE08";
+            case BossType.Vampire:
+                return "\uD83E\uDDDB";
+            case BossType.Dragon:
+                return "\uD83D\uDC09";
+        }
+    }
+}
---
+++ DiscordBot/Commands/BossModules.cs
@@ -0,0 +1,67 @@
+﻿using DisCatSharp.CommandsNext;
+using DisCatSharp.CommandsNext.Attributes;
+using DisCatSharp.Entities;
+using DiscordBot.Boss;
+
+namespace DiscordBot.Commands;
+
+public class BossModules : BaseCommandModule
+{
+    private readonly BossMonster _bossMonster;
+
+    public BossModules()
+    {
+        var rand = new Random();
+        int bossType = rand.Next((int) BossType.Bat, (int) BossType.End);
+        _bossMonster = new BossMonster((BossType) bossType);
+    }
+    [Command, Aliases("ba"), Cooldown(1, 300, CooldownBucketType.User)]
+    public async Task BossAttack(CommandContext ctx)
+    {
+        var rand = new Random();
+
+        int damage = rand.Next(1, 101);
+        int hitCount = _bossMonster.HitCount;
+        string deadBossEmojiCode = _bossMonster.BossEmojiCode;
+        bool bIsKilled = _bossMonster.IsKilledByDamage(damage);
+
+        if (!bIsKilled)
+        {
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder()
+                .WithThumbnail("https://media.tenor.com/D5tuK7HmI3YAAAAi/dark-souls-knight.gif")
+                .WithColor(DiscordColor.HotPink)
+                .WithAuthor("\u2694\uFE0F " + ctx.Member.Username)
+                .AddField(new DiscordEmbedField("\uD83D\uDCA5 " + Convert.ToString(damage),
+                    _bossMonster.BossEmojiCode + " " + Convert.ToString(_bossMonster.CurrentHp) + "/" + Convert.ToString(_bossMonster.CurrentMaxHp), false));
+        
+            await ctx.RespondAsync(embedBuilder);
+        }
+        else
+        {
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder()
+                .WithThumbnail("https://media.tenor.com/mV5aSB_USt4AAAAi/coins.gif")
+                .WithColor(DiscordColor.Gold)
+                .WithAuthor("\uD83C\uDF8A " + ctx.Member.Username)
+                .AddField(new DiscordEmbedField("\u2620\uFE0F " + deadBossEmojiCode + " \u2620\uFE0F", "\u2694\uFE0F " + Convert.ToString(hitCount+1), false));
+        
+            var message = await ctx.RespondAsync(embedBuilder);
+
+            if( message != null )
+            {
+                await message.PinAsync();
+            }
+        }
+    }
+    [Command, Aliases("bi")]
+    public async Task BossInfo(CommandContext ctx)
+    {
+        DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder()
+            .WithThumbnail("https://i.pinimg.com/originals/22/ef/7e/22ef7eba94bc378be084e59e72eb7b25.jpg")
+            .WithColor(DiscordColor.Orange)
+            //.WithAuthor(_bossMonster.BossEmojiCode)
+            .AddField(new DiscordEmbedField(_bossMonster.BossEmojiCode, "----", false))
+            .AddField(new DiscordEmbedField("\u2665\uFE0F " + _bossMonster.CurrentHp + "/" + _bossMonster.CurrentMaxHp, "\u2694\uFE0F " + _bossMonster.HitCount, false));
+        
+        await ctx.RespondAsync(embedBuilder);
+    }
+}
